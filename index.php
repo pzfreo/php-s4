edd0d6addb84e12e728589399cf6d7fbf809574c
Paul Fremantle <pzfreo@github.com>
2014-03-18 14:05:02 +0000

    added index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<HTML>
+<HEAD>
+<TITLE>
+<?php 
+	echo "Date" . date('l jS \of F Y h:i:s A');
+?>
+</TITLE>
+</HEAD>
+<BODY>
+<H1>Sample PHP App running on Stratos2 at EclipseCon</H1>
+
+<br>
+This demo app is very very basic and is using 
+<?php 
+echo 'Current PHP version: ' . phpversion(); 
+?>
+</BODY>
+</HTML>
